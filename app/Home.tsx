a6aaa05aed99e14eab4fe7c58025e41ccd49743c
CodeSandbox <support@codesandbox.io>
2023-02-06 20:15:54 +0000

    Initial commit

---
+++ app/Home.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button, Stack, Typography } from "@mui/material";
+
+export const Home = () => (
+  <Stack sx={{ minHeight: "100vh", padding: "64px" }}>
+    <Stack
+      sx={{ flexGrow: 1, justifyContent: "center", alignItems: "center" }}
+      component="main"
+    >
+      <Typography variant="h5">main content</Typography>
+      <Button variant="contained">test</Button>
+    </Stack>
+    <Stack
+      direction="row"
+      sx={{ justifyContent: "space-between" }}
+      component="footer"
+    >
+      <Typography>footer</Typography>
+      <Typography>stuff</Typography>
+    </Stack>
+  </Stack>
+);
